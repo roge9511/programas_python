d0dbe5e1133802da9fde5e21178c416431493f70
erodriguez935 <74390913+roge9511@users.noreply.github.com>
2024-02-29 22:43:00 -0600

    Add files via upload

---
+++ ejerciciospyhton.py
@@ -0,0 +1,19 @@
+"""
+#ejercicio2
+a=input("cual es tu nombre: ")
+b=input("cuantas ventas hiciste: ")
+total=round(int(b)*13/100,2)
+print(f"hola {a} tus ventas son {b} con el 13% es un total de ${total}")
+"""
+frase = "Si la implementación es difícil de explicar, puede que sea una mala idea."
+a = frase.replace("difícil","fácil",)
+a= frase.replace("mala","buena",)
+print(a)
+print("hola")
+print("esta es una prueba de ejercicio")
+print("1")
+
+list =[1,2,3]
+list.pop()
+print(list)
+
